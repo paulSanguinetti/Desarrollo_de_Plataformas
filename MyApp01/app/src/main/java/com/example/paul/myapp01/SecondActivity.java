11c0a1633b871fa6711cefb248919fa9d362d9ef
Paul Sanguinetti <paul.sanguinetti@ucsp.edu.pe>
2017-06-08 00:34:42 -0500

    new

---
+++ MyApp01/app/src/main/java/com/example/paul/myapp01/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.paul.myapp01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by paul on 07-Jun-17.
+ */
+
+public class SecondActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
---
+++ MyApp01/app/src/main/java/com/example/paul/myapp01/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.paul.myapp01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by paul on 07-Jun-17.
+ */
+
+public class ThirdActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+
+}
